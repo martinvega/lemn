06d8985822646b658ffb740abfbb389cce457e08
Martín Vega <martin.vega7@gmail.com>
2012-08-11 13:26:59 -0300

    De x0y0 a lemn nuevamente

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ENDORFINA::Application.config.session_store :cookie_store, :key => '_endorfina_session'
+LEMN::Application.config.session_store :cookie_store, :key => '_lemn_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
-X0Y0::Application.routes.draw do
+LEMN::Application.routes.draw do
   devise_for :users
-  
+
   resources :users do
     member do
       get :edit_profile
       put :update_profile
     end
   end
-  
+
   root to: 'users#index'
 end
