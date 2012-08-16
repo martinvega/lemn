b323062b09e631b769a4db5f4332d28db71b3df6
Martín Vega <martin.vega7@gmail.com>
2012-08-15 21:28:48 -0300

    Scopes, validaciones, relaciones, etc, en asistencias

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,3 +1,20 @@
 class Assistance < ActiveRecord::Base
-  attr_accessible :date, :partner_id
+  has_paper_trail
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :date
+
+  # Default order
+  default_scope order('date DESC')
+
+  # Validations
+  validates :date, :partner_id, :user_id, :presence => true
+  validates_date :date, :on_or_before => lambda { Date.current }, :allow_nil => true, :allow_blank => true
+
+  # Relations
+  belongs_to :user
+  belongs_to :partner
+
 end
+
+
