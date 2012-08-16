bf61dbf1b36ba0ab21594b6271300459e27017e4
Martín Vega <martin.vega7@gmail.com>
2012-08-15 21:31:01 -0300

    Scopes, validaciones, relaciones, etc, en pagos

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,21 @@
 class Payment < ActiveRecord::Base
-  attr_accessible :amount, :concept, :date, :partner_id
+  has_paper_trail
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :amount, :concept, :date
+
+  # Default order
+  default_scope order('date DESC')
+
+  # Validations
+  validates :date, :amount, :concept, :partner_id, :user_id, :presence => true
+  validates :concept, :length => { :maximum => 255 }, :allow_nil => true, :allow_blank => true
+  validates :amount, :numericality => { :maximum => 9999 }, :allow_nil => true, :allow_blank => true
+  validates :date, :timeliness => {:on_or_before => lambda { Date.current }, :type => :date},
+    :allow_nil => true, :allow_blank => true
+
+  # Relations
+  belongs_to :user
+  belongs_to :partner
+
 end
