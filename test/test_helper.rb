96742dc7f57d4b89587d9ff63554c3d31e5d873c
Martín Vega <martin.vega7@gmail.com>
2012-08-25 19:17:38 -0300

    Test helper a 1.8 ;)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
-  
+
   def error_message_from_model(model, attribute, message, extra = {})
     ::ActiveModel::Errors.new(model).generate_message(attribute, message, extra)
   end
@@ -26,7 +26,7 @@ class ActionDispatch::IntegrationTest
 
   # Stop ActiveRecord from wrapping tests in transactions
   self.use_transactional_fixtures = false
-  
+
   setup do
     Capybara.default_driver = :selenium
   end
@@ -39,29 +39,29 @@ class ActionDispatch::IntegrationTest
     # Revert Capybara.current_driver to Capybara.default_driver
     Capybara.use_default_driver
   end
-  
+
   def login
-    user = Fabricate(:user, password: '123456')
-    
+    user = Fabricate(:user, :password => '123456')
+
     visit new_user_session_path
-    
+
     assert_page_has_no_errors!
-    
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: '123456'
-    
+
+    fill_in 'user_email', :with => user.email
+    fill_in 'user_password', :with => '123456'
+
     find('.btn.btn-primary').click
-    
+
     assert_equal root_path, current_path
-    
+
     assert_page_has_no_errors!
     assert page.has_css?('.alert')
-    
+
     within '.alert' do
       assert page.has_content?(I18n.t('devise.sessions.signed_in'))
     end
   end
-  
+
   def assert_page_has_no_errors!
     assert page.has_no_css?('#unexpected_error')
   end
