d98869dac012ac19d96f664e34727eadb749038a
Martín Vega <martin.vega7@gmail.com>
2012-08-26 11:40:40 -0300

    Correcciones en fabricator de usuario

--- test/fabricators/user_fabricator.rb
+++ test/fabricators/user_fabricator.rb
@@ -1,10 +1,13 @@
 Fabricator(:user) do
-  name { Faker::Name.first_name }
-  lastname { Faker::Name.last_name }
-  email { |attrs|
+  name      { Faker::Name.first_name }
+  lastname  { Faker::Name.last_name }
+  user      { |attrs|
+    "#{attrs[:name]}_#{attrs[:lastname]}"
+  }
+  email     { |attrs|
     Faker::Internet.email([attrs[:name], sequence(:user_id)].join(' '))
   }
-  password { Faker::Lorem.sentence }
+  password  { Faker::Lorem.sentence }
   password_confirmation { |attrs| attrs[:password] }
   role :admin
 end
