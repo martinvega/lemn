ad450ca33858af32fedc0e534733997d5d591aa4
Martín Vega <martin.vega7@gmail.com>
2012-08-26 17:25:00 -0300

    Pruebas funcionales de socios

--- test/functional/partners_controller_test.rb
+++ test/functional/partners_controller_test.rb
@@ -2,44 +2,81 @@ require 'test_helper'
 
 class PartnersControllerTest < ActionController::TestCase
   setup do
-    @partner = partners(:one)
+    @partner = Fabricate(:partner)
+    @user = @partner.user
   end
 
   test "should get index" do
+    sign_in @user
+
     get :index
     assert_response :success
     assert_not_nil assigns(:partners)
+    assert_not_nil assigns(:partners)
+    assert_select '#unexpected_error', false
+    assert_template 'partners/index'
   end
 
   test "should get new" do
+    sign_in @user
+
     get :new
     assert_response :success
+    assert_not_nil assigns(:partner)
+    assert_select '#unexpected_error', false
+    assert_template 'partners/new'
   end
 
   test "should create partner" do
+    sign_in @user
+
     assert_difference('Partner.count') do
-      post :create, :partner => { :address => @partner.address, :admission_date => @partner.admission_date, :birth_date => @partner.birth_date, :document => @partner.document, :email => @partner.email, :lastname => @partner.lastname, :movil_phone => @partner.movil_phone, :name => @partner.name, :phone => @partner.phone }
+      post :create, :user_id => @user.id, :partner => Fabricate.attributes_for(:partner).slice(
+        *Partner.accessible_attributes
+      )
     end
 
     assert_redirected_to partner_path(assigns(:partner))
   end
 
   test "should show partner" do
+    sign_in @user
+
     get :show, :id => @partner
     assert_response :success
+    assert_not_nil assigns(:partner)
+    assert_select '#unexpected_error', false
+    assert_template 'partners/show'
   end
 
   test "should get edit" do
+    sign_in @user
+
     get :edit, :id => @partner
     assert_response :success
+    assert_not_nil assigns(:partner)
+    assert_select '#unexpected_error', false
+    assert_template 'partners/edit'
   end
 
   test "should update partner" do
-    put :update, :id => @partner, :partner => { :address => @partner.address, :admission_date => @partner.admission_date, :birth_date => @partner.birth_date, :document => @partner.document, :email => @partner.email, :lastname => @partner.lastname, :movil_phone => @partner.movil_phone, :name => @partner.name, :phone => @partner.phone }
+    sign_in @user
+
+    assert_no_difference 'Partner.count' do
+      put :update, :user_id => @user.id, :id => @partner, :partner => Fabricate.attributes_for(
+        :partner, :name => 'updated_name'
+      ).slice(
+        *Partner.accessible_attributes
+      )
+    end
+
     assert_redirected_to partner_path(assigns(:partner))
+    assert_equal 'updated_name', @partner.reload.name
   end
 
   test "should destroy partner" do
+    sign_in @user
+
     assert_difference('Partner.count', -1) do
       delete :destroy, :id => @partner
     end
